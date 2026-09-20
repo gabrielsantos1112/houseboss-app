696137cfce06cbae6c60209e6ce070400e5c121d
gabrielsantos1112 <gabrieloliveira0094@gmail.com>
2026-09-20 20:48:32 -0300

    Commit inicial

---
+++ src/main/java/br/com/houseboss/houseboss_app/HousebossAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.houseboss.houseboss_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousebossAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousebossAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/houseboss/houseboss_app/HousebossAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.houseboss.houseboss_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousebossAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
